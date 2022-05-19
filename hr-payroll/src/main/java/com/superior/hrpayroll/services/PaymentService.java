9bce66c828a12a802912ee384aa720f6cedd62e8
joaowilian01 <joaowilian01@gmail.com>
2022-05-19 20:21:34 -0300

    Ribbon load balacing

--- hr-payroll/src/main/java/com/superior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/superior/hrpayroll/services/PaymentService.java
@@ -4,13 +4,7 @@ import com.superior.hrpayroll.entities.Payment;
 import com.superior.hrpayroll.entities.Worker;
 import com.superior.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
@@ -20,7 +14,7 @@ public class PaymentService {
 
     public Payment getPayment(long workerId, int days) {
         Worker worker = workerFeignClient.findById(workerId).getBody();
-        
+
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
